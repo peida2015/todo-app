c5403a6c6778ee89a6b552efd26e7d46a0ddd907
App Academy Student <student@appacademy.io>
2015-12-08 14:50:58 -0800

    Added Delete button to each item listed

--- frontend/components/todo_list_item.jsx
+++ frontend/components/todo_list_item.jsx
@@ -1,11 +1,27 @@
 var React = require('react');
+var TodoStore = require('../stores/todo_store');
 
 var TodoListItem = React.createClass({
+  addDeleteButton: function () {
+    return (
+      <button onClick={ this.handleDeleteClick }>Delete</button>
+    );
+  },
+
+  handleDeleteClick: function (e) {
+    e.preventDefault();
+
+    var id = this.props.item.id;
+
+    TodoStore.destroy(id);
+  },
+
   render: function () {
     return (
       <div>
         <div>{ this.props.item.title }</div>
         <div>{ this.props.item.body }</div>
+        <div>{ this.addDeleteButton() }</div>
       </div>
     );
   }
