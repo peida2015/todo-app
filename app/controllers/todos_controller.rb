9a4dfccff9c182fcd9b6a85cfe651628ea24bf9a
App Academy Student <student@appacademy.io>
2015-12-08 11:05:00 -0800

    Add Api::Todos and Static Pages controllers

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,27 @@
+class Api::TodosController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def todo_params
+
+  end
+end
