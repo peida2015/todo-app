ab2c8717d1ce5d0bdbb800f852988038e851dee4
App Academy Student <student@appacademy.io>
2015-12-08 15:09:27 -0800

    Add Done/Undo button compoenent

---
+++ frontend/components/done_button.jsx
@@ -0,0 +1,20 @@
+var React = require("react"),
+    TodoStore = require("../stores/todo_store");
+
+var DoneButton = React.createClass({
+  handleDone: function (e) {
+    e.preventDefault();
+    TodoStore.toggleDone(this.props.id);
+  },
+
+  render: function () {
+    var buttonText = (this.props.done ? "Undo" : "Done");
+
+    return (
+      <button className="btn btn-success"
+        onClick={ this.handleDone }>{ buttonText }</button>
+    );
+  }
+});
+
+module.exports = DoneButton;
--- frontend/components/todo_list_item.jsx
+++ frontend/components/todo_list_item.jsx
@@ -1,10 +1,12 @@
-var React = require('react');
-var TodoStore = require('../stores/todo_store');
+var React = require('react'),
+    TodoStore = require('../stores/todo_store'),
+    DoneButton = require("./done_button");
 
 var TodoListItem = React.createClass({
   addDeleteButton: function () {
     return (
-      <button onClick={ this.handleDeleteClick }>Delete</button>
+      <button className="btn btn-success"
+        onClick={ this.handleDeleteClick }>Delete</button>
     );
   },
 
@@ -22,6 +24,8 @@ var TodoListItem = React.createClass({
         <div>{ this.props.item.title }</div>
         <div>{ this.props.item.body }</div>
         <div>{ this.addDeleteButton() }</div>
+        <DoneButton done={ this.props.item.done }
+          id={ this.props.item.id }/>
       </div>
     );
   }
