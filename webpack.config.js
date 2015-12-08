9c2047dd077ed5c858b1ea3234df4b6a4270142f
App Academy Student <student@appacademy.io>
2015-12-08 10:59:07 -0800

    finished adding todo model

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+var path = require("path");
+
 module.exports = {
   context: __dirname,
   entry: "./frontend/todo.jsx",
   output: {
-    path: "./app/assets/javascripts",
-    filename: "bundle.js"
+    path: path.join(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
   module: {
     loaders: [
